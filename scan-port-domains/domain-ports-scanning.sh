8b8dc8826f2faaef3a330dd2b99c5169e470165d
Agustina Fassina | ENG <agustinafassina@gmail.com>
2025-10-31 13:06:02 -0300

    add date in the csv result

--- scan-port-domains/domain-ports-scanning.sh
+++ scan-port-domains/domain-ports-scanning.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo apt-get install -y jq
+sudo apt-get install -y nmap
 
 json_file="records.json"
 domains_file="domains.txt"
@@ -9,7 +10,7 @@ ports_to_scan="21,22,23,25,53,80,110,143,443,993,995,3389,3306,5432,5900,8080,84
 
 jq -r '.[] | .Records[]?.Name' "$json_file" > "$domains_file"
 
-echo "Domain,Ports Open" > "$result_csv"
+echo "Domain,Ports Open,Scan Time" > "$result_csv"
 
 while IFS= read -r domain
 do
@@ -20,7 +21,11 @@ do
     ports="None"
   fi
 
-  echo "$domain,\"$ports\"" >> "$result_csv"
+  # Obtener la fecha y hora actual en formato legible
+  scan_time=$(date '+%Y-%m-%d %H:%M:%S')
+
+  # Escribir en el CSV incluyendo la hora
+  echo "$domain,\"$ports\",$scan_time" >> "$result_csv"
 done < "$domains_file"
 
 echo "Completed scan. results in $result_csv"
